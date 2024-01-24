607ad3026c16e7569758cb07c9d533ada4f2c4ab
ldu <dldnr3043@naver.com>
2024-01-24 12:45:23 +0900

    vue version 3->2 로 변경

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+  transpileDependencies: true
 })
-
-module.exports = {
-  // css: {
-  //   loaderOptions: {
-  //     scss: {
-  //       prependData: '@import @/assets/sass/login.scss'
-  //     }
-  //   }
-  // },
-
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:40000',
-        changeOrigin: true,
-      },
-    },
-  },
-
-  pluginOptions: {
-    vuetify: {}
-  }
-};
