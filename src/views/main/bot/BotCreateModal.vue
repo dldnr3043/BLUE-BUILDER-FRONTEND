797ee87b7186c81a841526d5aa7198cb8ba86df2
ldu <dldnr3043@naver.com>
2024-02-19 15:25:18 +0900

    modal template

---
+++ src/views/main/bot/BotCreateModal.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app>
+        <div class="bot-list-table">
+            
+        </div>
+    </v-app>
+</template>
+
+<script>
+import { mixin } from '@/mixin/mixin'
+
+export default {
+    mixins: [
+        mixin
+    ],
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+
+    mounted() {
+    },
+}
+</script>
+
+<style>
+</style>
